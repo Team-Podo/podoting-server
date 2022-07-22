2c2f719878d1a5dce1d577ef238bbc6761c74ca9
nohkwanok <cloq@kakao.com>
2022-07-22 16:11:55 +0900

    json 순서를 위한 MapSlice 정의

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"path/filepath"
@@ -11,6 +14,30 @@ var (
 	_, b, _, _ = runtime.Caller(0)
 	basePath   = filepath.Dir(b)
 )
+
+type MapItem struct {
+	Key, Value interface{}
+}
+
+type MapSlice []MapItem
+
+func (ms MapSlice) MarshalJSON() ([]byte, error) {
+	buf := &bytes.Buffer{}
+	buf.Write([]byte{'{'})
+	for i, mi := range ms {
+		b, err := json.Marshal(&mi.Value)
+		if err != nil {
+			return nil, err
+		}
+		buf.WriteString(fmt.Sprintf("%q:", fmt.Sprintf("%v", mi.Key)))
+		buf.Write(b)
+		if i < len(ms)-1 {
+			buf.Write([]byte{','})
+		}
+	}
+	buf.Write([]byte{'}'})
+	return buf.Bytes(), nil
+}
 
 func RootPath() string {
 	return basePath[:len(basePath)-5]
