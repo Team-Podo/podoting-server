4d887092dcbcdf70bf86310f0969bd0166e6b710
nohkwanok <cloq@kakao.com>
2022-09-14 15:13:11 +0900

    schedule 테스트 라우팅 추가

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Team-Podo/podoting-server/endpoints/admin/product"
 	"github.com/Team-Podo/podoting-server/endpoints/admin/schedule"
 	"github.com/Team-Podo/podoting-server/endpoints/musical"
+	"github.com/Team-Podo/podoting-server/utils"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -79,6 +80,54 @@ func Routes(r *gin.Engine) {
 	musicalGroup := r.Group("/musical")
 	{
 		musicalGroup.GET("/:id", musical.Find)
+		musicalGroup.GET("/:id/schedules/:scheduleUUID/seats", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"backgroundImage": "https://podoting.com/images/seat-background.png",
+				"seats": []utils.MapSlice{
+					utils.BuildMapSliceByMap(map[string]any{
+						"uuid": "a6c6a191-7ef9-4695-b89b-d76bf2e31d08",
+						"point": utils.BuildMapSliceByMap(map[string]any{
+							"x": 530.2091,
+							"y": 620.1124,
+						}),
+						"name": "I 열 11번 좌석",
+						"grade": utils.BuildMapSliceByMap(map[string]any{
+							"id":   102,
+							"name": "VIP",
+						}),
+						"price": 99000,
+						"color": "#7748F4",
+					}),
+					utils.BuildMapSliceByMap(map[string]any{
+						"uuid": "a6c6a191-7ef9-4695-b89b-d76bf2e31d09",
+						"point": utils.BuildMapSliceByMap(map[string]any{
+							"x": 534.2091,
+							"y": 620.1124,
+						}),
+						"name": "I 열 12번 좌석",
+						"grade": utils.BuildMapSliceByMap(map[string]any{
+							"id":   102,
+							"name": "VIP",
+						}),
+						"price": 99000,
+						"color": "#7748F4",
+					}),
+					utils.BuildMapSliceByMap(map[string]any{
+						"uuid": "a6c6a191-7ef9-4695-b89b-d76bf2e31d10",
+						"point": utils.BuildMapSliceByMap(map[string]any{
+							"x": 538.2091,
+							"y": 620.1124,
+						}),
+						"name": "I 열 13번 좌석",
+						"grade": utils.BuildMapSliceByMap(map[string]any{
+							"id":   102,
+							"name": "VIP",
+						}),
+						"price": 99000,
+						"color": "#7748F4",
+					}),
+				},
+			})
+		})
 	}
-
 }
